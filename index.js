50f3b7e3670b226699de1ef7f7d3abb1fbc6cfa7
Clifton <clifga@gmail.com>
2023-03-19 22:37:32 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers (array) {
+    return array.slice(0,2)
+}
+
+function returnLastTwoDrivers(array) {
+    return array.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers ]
+
+function createFareMultiplier (num) {
+    return function multipy(fare) {
+      return num * fare
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(array, callback) {
+    return callback(array)
+
+}
